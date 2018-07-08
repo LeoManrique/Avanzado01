860679a5cc34e71d42d6bdec1e340bbd93398357
Leonardo Manrique <leonardomanrique43@gmail.com>
2018-07-08 16:40:42 -0500

    bienvenida

--- app/src/main/java/com/example/leonardo/avanzado01/activities/NotesActivity.java
+++ app/src/main/java/com/example/leonardo/avanzado01/activities/NotesActivity.java
@@ -12,13 +12,16 @@ import com.example.leonardo.avanzado01.R;
 public class NotesActivity extends AppCompatActivity {
     private TextView bienvenido;
     private SharedPreferences sharedPreferences;
+    String nombre;
+    String bienvenida;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         bienvenido = findViewById(R.id.bienvenido);
-        //bienvenido.setText("Bienvenido "+sharedPreferences.getString("firstName", ""));
-        Toast.makeText(this, sharedPreferences.getString("firstName", ""), Toast.LENGTH_SHORT).show();
+        nombre = sharedPreferences.getString("firstName", "");
+        bienvenida = "Bienvenido "+nombre;
+        bienvenido.setText(bienvenida);
     }
 }
